7cbc37789e160c61f3a8400c0a4df055d9d9e3c5
段昊辰 <duanhaochen@126.com>
2020-03-20 15:28:59 +0800

    build: 修改编译脚本位置

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const childProcess = require('child_process');
-const path = require('path');
 const os = require('os');
 const gulp = require('gulp');
 const rename = require('gulp-rename');
@@ -7,9 +6,7 @@ const chalk = require('chalk');
 
 function buildProject() {
   return new Promise((resolve, reject) => {
-    const child = childProcess.spawn('npm', ['run', os.platform() === 'win32' ? 'build:win' : 'build:unix'], {
-      cwd: path.join(__dirname, '..')
-    });
+    const child = childProcess.spawn('npm', ['run', os.platform() === 'win32' ? 'build:win' : 'build:unix']);
 
     child.stdout.on('data', (data) => {
       console.log(data.toString());
@@ -24,6 +21,7 @@ function buildProject() {
     });
 
     child.on('error', (err) => {
+      console.error(err);
       reject(err);
     });
   });
@@ -31,23 +29,23 @@ function buildProject() {
 
 /* 拷贝template */
 function copyTemplate() {
-  return gulp.src('../template/**/*.*')
-    .pipe(gulp.dest('../dist/github-cli/template'))
+  return gulp.src('template/**/*.*')
+    .pipe(gulp.dest('dist/github-cli/template'))
 }
 
 /* 拷贝配置文件 */
 function copyConfigFile() {
-  return gulp.src('../config-example.json')
+  return gulp.src('config-example.json')
     .pipe(rename(function(p) {
       p.basename = 'config';
     }))
-    .pipe(gulp.dest('../dist/github-cli'))
+    .pipe(gulp.dest('dist/github-cli'))
 }
 
 /* 拷贝许可证 */
 function copyLicense() {
-  return gulp.src('../LICENSE')
-    .pipe(gulp.dest('../dist/github-cli'))
+  return gulp.src('LICENSE')
+    .pipe(gulp.dest('dist/github-cli'))
 }
 
 exports.default = gulp.parallel(buildProject, copyTemplate, copyConfigFile, copyLicense);
